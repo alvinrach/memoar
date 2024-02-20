a29b0e2de5ddcf867eca2956c75912dc5f618c6b
alvinrach <alvinracht@gmail.com>
2024-02-20 23:43:18 +0700

    Semi Final Form

--- pytest.py
+++ pytest.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_response():
-    response = requests.get('http://localhost:8000/doc')
+    response = requests.get('http://localhost:8000/docs')
     print(response.status_code)
     assert response.status_code == 200
     print(1)
