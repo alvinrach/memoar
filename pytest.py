cb73d353942d43830d817d302b7d6dd33113ee08
alvinrach <alvinracht@gmail.com>
2024-02-20 23:29:55 +0700

    Intended error

--- pytest.py
+++ pytest.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_response():
-    response = requests.get('http://localhost:8000/docs')
+    response = requests.get('http://localhost:8000/doc')
     print(response.status_code)
     assert response.status_code == 200
     print(1)
