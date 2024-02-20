c3dd55e4f229f00dd51d049d9330a8d3b812a93c
alvinrach <alvinracht@gmail.com>
2024-02-21 00:15:01 +0700

    time

--- pytest.py
+++ pytest.py
@@ -1,6 +1,8 @@
 import requests
+import time
 
 def test_response():
+    time.sleep(10)
     response = requests.get('http://localhost:8000/docs')
     print(response.status_code)
     assert response.status_code == 200
